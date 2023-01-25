1514c3ec207925b5538ec71f250155086c957056
odionjulius7 <odionjulius7@gmail.com>
2023-01-25 03:28:18 +0100

    bd

--- src/components/ProgramProviderComponents/shared/SingleProgramCandidateInfo/index.tsx
+++ src/components/ProgramProviderComponents/shared/SingleProgramCandidateInfo/index.tsx
@@ -4,30 +4,29 @@ import CandidateScore from "./CandidateScore";
 import CandidateTabs from "./CandidateTabs";
 import CandidateTabsDisplay from "./CandidateTabsDisplay";
 import { useState } from "react";
-import CandidateVideo from "./CandidateVideo";
+// import CandidateVideo from "../";
 import CandidateEvaluation from "./CandidateEvaluation";
-
+// CandidateVideo
 const SingleProgramCandidateNav = () => {
-  const [currentTab, setCurrentTab] = useState<number>(1);
+	const [currentTab, setCurrentTab] = useState<number>(1);
 
-
-  return (
-    <Box sx={{ width: "100%" }}>
-      <Stack direction="row" justifyContent="space-between" my={2}>
-        <CandidateScore score={6.6} />
-        <CandidateCTA />
-      </Stack>
-      <CandidateTabs
-        currentLabel={currentTab}
-        labels={["Profile", "Video", "Evaluation", "Notes", "Placement", "Log"]}
-        onChange={(num) => {
-          setCurrentTab(num);
-        }}
-      />
-      {/* <CandidateVideo /> */}
-      <CandidateEvaluation />
-    </Box>
-  );
+	// return (
+	// 	<Box sx={{ width: "100%" }}>
+	// 		<Stack direction="row" justifyContent="space-between" my={2}>
+	// 			<CandidateScore score={6.6} />
+	// 			<CandidateCTA />
+	// 		</Stack>
+	// 		<CandidateTabs
+	// 			currentLabel={currentTab}
+	// 			labels={["Profile", "Video", "Evaluation", "Notes", "Placement", "Log"]}
+	// 			onChange={(num) => {
+	// 				setCurrentTab(num);
+	// 			}}
+	// 		/>
+	// 		{/* <CandidateVideo /> */}
+	// 		<CandidateEvaluation />
+	// 	</Box>
+	// );
 
 	return (
 		<Box sx={{ width: "100%" }}>
@@ -42,9 +41,10 @@ const SingleProgramCandidateNav = () => {
 					setCurrentTab(num);
 				}}
 			/>
-			<CandidateTabsDisplay currentTab={currentTab} />
+			{currentTab === 2 && <CandidateTabsDisplay currentTab={currentTab} />}
+
+			{currentTab === 3 && <CandidateEvaluation />}
 		</Box>
 	);
-
 };
 export default SingleProgramCandidateNav;
