de7ff7871e2cdafbf26c20a62861ece67d4cc798
ehiz_briel <ehisucre@gmail.com>
2023-01-25 03:38:12 +0100

    added notes tab

--- src/components/ProgramProviderComponents/shared/SingleProgramCandidateInfo/CandidateTabsDisplay/index.tsx
+++ src/components/ProgramProviderComponents/shared/SingleProgramCandidateInfo/CandidateTabsDisplay/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import VideoTab from "./VideoTab";
-// import VideoTab from "./VideoTab"
+import NotesTab from "./NotesTab";
 
 type Props = {
 	currentTab: number;
 };
 
 export default function Index({ currentTab }: Props) {
-	return <>{currentTab == 2 && <VideoTab />}</>;
+	return (
+		<>
+			{currentTab === 2 && <VideoTab />}
+			{currentTab === 4 && <NotesTab />}
+			{/* {currentTab == 2 && <VideoTab />} */}
+		</>
+	);
 }
